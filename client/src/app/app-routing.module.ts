2ff784607348348115add92e71886e2d929f75b7
AlbanisGonzalez <albanisy@gmail.com>
2023-05-20 14:08:38 +0200

    create register module and routing flow from register to petgram

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FirstPageComponent } from './register/first-page/first-page.component';
-import { LoginComponent } from './register/login/login.component';
-import { SignupComponent } from './register/signup/signup.component';
+
 
 const routes: Routes = [
-  {path:"",component:FirstPageComponent},
-  {path:"login",component:LoginComponent},
-  {path:"signup",component:SignupComponent},
+  {path:"",loadChildren:()=> import("./register/register.module").then(m => m.RegisterModule)},
   {path:"petgram",loadChildren:()=> import("./petgram/petgram.module").then(m => m.PetgramModule)},
 
 ];
---
+++ client/src/app/register/register.routing.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DataUserComponent } from './data-user/data-user.component';
+import { DataPetComponent } from './data-pet/data-pet.component';
+
+const routes: Routes = [
+  { path: '', component: FirstPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', children: [
+    { path: '', component: SignupComponent },
+    { path: 'data-user', component: DataUserComponent },
+    { path: 'data-pet', component: DataPetComponent },
+  ] },
+];
+
+export const RegisterRoutes = RouterModule.forChild(routes);
