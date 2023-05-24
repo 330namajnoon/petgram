d491760aebb66513eddb1e843072805f9a0d2076
330namajnoon <sina.majnoonhjk@gmail.com>
2023-05-24 14:30:09 +0200

    comments

--- client/src/app/petgram/home/comments/comments.component.ts
+++ client/src/app/petgram/home/comments/comments.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AppService } from './../../../app.service';
+import { Component,Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IComments } from 'src/app/interfaces/IComments';
+import { IStory } from 'src/app/interfaces/IStory';
+import { HomeService } from '../home.service';
+import { ICommentsData } from 'src/app/interfaces/ICommentsData';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss']
 })
-export class CommentsComponent {
+export class CommentsComponent  {
+  commentsData!:ICommentsData;
+  constructor(private actRouter:ActivatedRoute,private appService:AppService,private homeService:HomeService){
+    this.actRouter.params.subscribe(params => {
+      let id = params["id"];
+      this.commentsData = {story:this.homeService.get(id).getStory(),comments:this.homeService.get(id).getComments()};
+    })
+  }
+
+  getDevice(): string {
+      return this.appService.getDevice();
+  }
 
 }
