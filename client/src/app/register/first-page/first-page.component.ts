2977ff30cc80890b382e1da200321324c4069d3d
wini1011 <wini.vma@gmail.com>
2023-05-30 09:34:20 +0200

    pagina principal

--- client/src/app/register/first-page/first-page.component.ts
+++ client/src/app/register/first-page/first-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-first-page',
@@ -7,11 +8,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./first-page.component.scss']
 })
 export class FirstPageComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router,private appS:AppService) {}
 
   redirectToLogin(){
     this.router.navigate(['/login'])
   }
 
+  getDevice():string {
+    return this.appS.getDevice();
+  }
 
 }
