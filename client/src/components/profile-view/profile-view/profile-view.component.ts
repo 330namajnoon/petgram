83fe7dfffa9622e1416446b413b5e65046469332
330namajnoon <sina.majnoonhjk@gmail.com>
2023-06-12 12:43:18 +0200

    profile wiew

---
+++ client/src/components/profile-view/profile-view/profile-view.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { HomeService } from 'src/services/home.service';
+import { AppService } from 'src/services/app.service';
+import { IUserData } from 'src/ss/interfaces/IUserData';
+import { ProfileViewService } from 'src/services/profile-view.service';
+import { IStoryAdress } from 'src/ss/interfaces/IStoryAdress';
+import { AppServiceEx } from 'src/extends/AppServiceEx';
+import { IFollower } from 'src/interfaces/IFollower';
+import { IUser } from 'src/interfaces/IUser';
+@Component({
+  selector: 'app-profile-view',
+  templateUrl: './profile-view.component.html',
+  styleUrls: ['./profile-view.component.scss']
+})
+export class ProfileViewComponent extends AppServiceEx implements OnInit {
+  constructor(private profileService:ProfileViewService,private acRoute:ActivatedRoute,private router:Router,private homeService:HomeService,appService:AppService) {
+    super(appService)
+    router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+
+        let state = router.getCurrentNavigation()?.extras.state;
+        if(state) {
+          let data = state as {user:string};
+          this.profileService.downloadProfileData(data.user);
+
+        }
+      }
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  getMyProfile():boolean {
+    if(this.getProfileData().id == this.getUser().id) {
+      return false;
+    }else {
+      return true;
+    }
+  }
+  getProfileData():IUser {
+
+    return this.profileService.getProfileData();
+  }
+
+
+  storysFilter(pet:string):IStoryAdress[] {
+    let storys:IStoryAdress[] = [];
+    // this.getProfileData().storys.forEach(s => {
+    //   if(s.pet == pet) storys.push(s);
+    // })
+    return storys
+  }
+
+  pendingFollowersSearch():boolean {
+    // let follower:IFollower|undefined = this.profileService.getProfileData()..find(f => f.user == this.getUser().user);
+    // if(follower) {
+    //   return true;
+    // }else {
+    //   return false
+    // }
+    return true
+  }
+
+  followersSearch():boolean {
+    // let follower:IFollower|undefined = this.profileService.getProfileData().followers.find(f => f.user == this.getUser().user);
+    // if(follower) {
+    //   return true;
+    // }else {
+    //   return false
+    // }
+    return false
+  }
+
+  follow():void {
+    // let {image,id,name,lastName} = this.getUser();
+    // let follower:IFollower = {
+    //   image,id,name,lastName
+    // }
+    // this.profileService.getProfileData().pendingFollowers.push(follower);
+  }
+}
