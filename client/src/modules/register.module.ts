1a5bca5f005c7a33d202021b9eac5c7f520faa40
AlbanisGonzalez <albanisy@gmail.com>
2023-06-18 13:39:39 +0200

    Unificación de formularios

--- client/src/modules/register.module.ts
+++ client/src/modules/register.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstPageComponent } from '../components/first-page/first-page.component';
 import { LoginComponent } from '../components/login/login.component';
-import { SignupComponent } from '../components/signup/signup.component';
 import { RegisterRoutes } from '../routers/register.routing';
-import { DataUserComponent } from '../components/data-user/data-user.component';
-import { DataPetComponent } from '../components/data-pet/data-pet.component';
 import { ReactiveFormsModule } from "@angular/forms";
 import { ValidatorComponent } from '../components/validator/validator.component';
 import { PasswordValidatorComponent } from '../components/password-validator/password-validator.component';
-import { ImagePetComponent } from 'src/components/image-pet/image-pet.component';
+import { ImagePetComponent } from 'src/components/register/image-pet/image-pet.component';
+import { DataPetComponent } from 'src/components/register/data-pet/data-pet.component';
+import { DataUserComponent } from 'src/components/register/data-user/data-user.component';
 
 @NgModule({
   declarations: [
     FirstPageComponent,
     LoginComponent,
-    SignupComponent,
     DataUserComponent,
     DataPetComponent,
     ValidatorComponent,
@@ -30,7 +28,6 @@ import { ImagePetComponent } from 'src/components/image-pet/image-pet.component'
   exports: [
     FirstPageComponent,
     LoginComponent,
-    SignupComponent
   ]
 })
 export class RegisterModule { }
